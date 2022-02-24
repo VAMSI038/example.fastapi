2edb06e5ca374253b3e4f3e3f71c28c44bf1d765
vamsi <vamsipothula038@gmail.com>
2022-02-24 11:42:10 +0530

    add msg

--- alembic/env.py
+++ alembic/env.py
@@ -3,8 +3,6 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from app.models import Base
 from app.config import settings
-
-
 from alembic import context
 
 # this is the Alembic Config object, which provides
